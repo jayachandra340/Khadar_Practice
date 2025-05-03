11bcf303a3e650f60d2638688f7a11ae8d374cab
jayachandra340 <jayachandrakosetti@gmail.com>
2025-05-03 22:06:58 +0530

    "updated by jc"

--- index.js
+++ index.js
@@ -28,3 +28,26 @@
 // console.log(result)
 
 //binary search code
+const prompt =require("prompt-sync")()
+let arr = [12, 34, 45, 5, 40, 30, 10, 38, 46, 4];
+let search = Number(prompt("Enter search number: "));
+arr = arr.sort((a, b) => a - b); // Sort the array in ascending order
+let f = 0;
+let l = arr.length - 1;
+
+function binarySearch(arr, search, f, l) {
+    while (f <= l) {
+        let m = Math.floor((f + l) / 2); // Calculate the middle index
+        if (arr[m] === search) {
+            return `Number ${search} found`;
+        } else if (arr[m] < search) {
+            f = m + 1; // Search in the right half
+        } else {
+            l = m - 1; // Search in the left half
+        }
+    }
+    return "Number not found";
+}
+
+let result = binarySearch(arr, search, f, l);
+console.log(result);
