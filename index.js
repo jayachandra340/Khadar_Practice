0db854e947279e793ff86f80bcf8455a933c5a3e
Khadar <21981a4213@raghuenggcollege.in>
2025-05-03 16:05:54 +0530

    add js file

---
+++ index.js
@@ -0,0 +1,11 @@
+//factorial
+function factorial(n) {
+    if (n < 0) {
+        return "undefined";
+    } else if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+console.log(factorial(5)); // Output: 120
