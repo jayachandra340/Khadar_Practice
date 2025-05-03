d11b08e74e403fc38831874245eb763c89b3a1ee
jayachandra340 <jayachandrakosetti@gmail.com>
2025-05-03 21:43:16 +0530

    updated by jc

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-//factorial
-function factorial(n) {
-    if (n < 0) {
-        return "undefined";
-    } else if (n === 0 || n === 1) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
-    }
-}
-console.log(factorial(10)); // Output: 3628800
+// //factorial
+// function factorial(n) {
+//     if (n < 0) {
+//         return "undefined";
+//     } else if (n === 0 || n === 1) {
+//         return 1;
+//     } else {
+//         return n * factorial(n - 1);
+//     }
+// }
+// console.log(factorial(10)); // Output: 3628800
+
+//hey man im jc git lo update chesthunna le uruko kangarupadaku
+
+// linear search code
+// const prompt = require("prompt-sync")()
+// let arr=[12,34,45,5,40,30,10,38,46,4]
+// let search = Number(prompt("enter search number: "))
+// function linearsort(arr, search) {
+//     for (let i of arr) {
+//         if (i === search) {
+//             return `number ${search} found at ${arr.indexOf(i)}`
+//         }
+//     }
+//     return "number not found"
+// }
+// let result = linearsort(arr,search)
+// console.log(result)
+
+//binary search code
